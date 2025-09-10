5e152481e23907680b5eedfcc69e596977942f64
Radit <raditya.amoret@gmail.com>
2025-09-10 08:12:23 +0700

    install

--- platform_bola_persija/settings.py
+++ platform_bola_persija/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raditya-amoret-platformbolapersija.p
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
