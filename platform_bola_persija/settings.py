9ab46f72cf073150f7731c357cea578befbae6ae
Radit <raditya.amoret@gmail.com>
2025-09-16 21:30:47 +0700

    fixing issues

--- platform_bola_persija/settings.py
+++ platform_bola_persija/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raditya-amoret-platformbolapersija.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "<url-deployment-pws-kamu>"
+    "<https://raditya-amoret-platformbolapersija.pbp.cs.ui.ac.id>"
 ]
 
 # Application definition
